3ee43100a92f72286c50546590d40bb7a1cf0940
crcrcr999 <crcrcr999@gmail.com>
2022-06-21 13:11:30 +0800

    播放结束曲

--- playmusic.py
+++ playmusic.py
@@ -24,6 +24,8 @@ def data_needed(filePath):
 
 #isworktime = lambda x,y: True if (x>=y[0] and x<y[1]) else False
 upanPath="/media/cr/MUSIC/"
+endMusicPath="end/范圣琦 - 回家 (萨克斯独奏).mp3"
+playEndMusic=False
 #workrange=[13,20]
 while True:
 	#nowhour = datetime.datetime.now().hour
@@ -31,11 +33,15 @@ while True:
 	if os.path.exists(upanPath):
 		if worktime.timecalendar():
 		#if nowminute<23:
+			playEndMusic=False
 			musiclist=data_needed(upanPath)
 			randomint=random.randint(0,len(musiclist))
 			speakpath = upanPath+musiclist[randomint]
 			play(speakpath)
-			time.sleep(1000)
+			time.sleep(1)
 	else:
+		if playEndMusic==False:
+			play(upanPath+endMusicPath)
+			playEndMusic=True
 		time.sleep(60)
 	
